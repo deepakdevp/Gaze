554d53fd7e102f21c63f0c6030c4b62cfb7266e7
deepak21 <deepakdevp@gmail.com>
2024-05-30 01:58:21 +0530

    middleware added

--- backend/main/middleware.py
+++ backend/main/middleware.py
@@ -1,11 +1,11 @@
 from django.utils.deprecation import MiddlewareMixin
 
 
+from django.utils.deprecation import MiddlewareMixin
+
+
 class XForwardedForMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
-        if 'REMOTE_ADDR' not in request.META:
-            try:
-                request.META['REMOTE_ADDR'] = request.META['HTTP_X_CLIENT_IP']
-            except:
-                request.META['REMOTE_ADDR'] = '1.1.1.1'
+        if "HTTP_X_FORWARDED_FOR" in request.META:
+            request.META["REMOTE_ADDR"] = request.META["HTTP_X_FORWARDED_FOR"]

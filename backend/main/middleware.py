b4d79745c737ede517b8b1282ffce92291715979
deepak21 <deepakdevp@gmail.com>
2024-05-30 01:56:23 +0530

    middleware added

--- backend/main/middleware.py
+++ backend/main/middleware.py
@@ -6,6 +6,6 @@ class XForwardedForMiddleware(MiddlewareMixin):
     def process_request(self, request):
         if 'REMOTE_ADDR' not in request.META:
             try:
-                request.META['REMOTE_ADDR'] = request.META['HTTP_X_REAL_IP']
+                request.META['REMOTE_ADDR'] = request.META['HTTP_X_CLIENT_IP']
             except:
                 request.META['REMOTE_ADDR'] = '1.1.1.1'

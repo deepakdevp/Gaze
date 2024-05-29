96f6a798c01ad14b3327176b77dac2d272f42951
deepak21 <deepakdevp@gmail.com>
2024-05-30 01:31:32 +0530

    middleware added

--- backend/main/middleware.py
+++ backend/main/middleware.py
@@ -2,8 +2,7 @@ from django.utils.deprecation import MiddlewareMixin
 
 
 class XForwardedForMiddleware(MiddlewareMixin):
+
     def process_request(self, request):
         if "HTTP_X_FORWARDED_FOR" in request.META:
-            request.META["HTTP_X_PROXY_REMOTE_ADDR"] = request.META["REMOTE_ADDR"]
-            parts = request.META["HTTP_X_FORWARDED_FOR"].split(",", 1)
-            request.META["REMOTE_ADDR"] = parts[0]
+            request.META["REMOTE_ADDR"] = request.META["HTTP_X_FORWARDED_FOR"]
